0a7feb9b522199d657dd699c83361e7b60fe3d0d
sujayyy <150987692+sujaytumu@users.noreply.github.com>
2025-08-25 14:03:07 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,3 @@
-// /** @type {import('tailwindcss').Config} */
-
-// export default {
-//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -28,5 +18,13 @@ export default {
       },
     },
   },
-  plugins: [],
-}
+  plugins: [require("daisyui")], // ✅ add DaisyUI plugin
+  daisyui: {
+    themes: [
+      "light","dark","cupcake","bumblebee","emerald","corporate","synthwave","retro",
+      "cyberpunk","valentine","halloween","garden","forest","aqua","lofi","pastel",
+      "fantasy","wireframe","black","luxury","dracula","cmyk","autumn","business",
+      "acid","lemonade","night","coffee","winter","dim","nord","sunset"
+    ],
+  },
+};
