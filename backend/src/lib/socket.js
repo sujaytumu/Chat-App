34ef350aef1ab8f61d311977e304ddf29efcd9c8
sujayyy <150987692+sujaytumu@users.noreply.github.com>
2025-08-30 19:59:16 +0530

    Update socket.js

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,3 +1,43 @@
+// import { Server } from "socket.io";
+// import http from "http";
+// import express from "express";
+
+// const app = express();
+// const server = http.createServer(app);
+
+// const io = new Server(server, {
+//   cors: {
+//     origin: ["http://localhost:5173"],
+//   },
+// });
+
+// export function getReceiverSocketId(userId) {
+//   return userSocketMap[userId];
+// }
+
+// // used to store online users
+// const userSocketMap = {}; // {userId: socketId}
+
+// io.on("connection", (socket) => {
+//   console.log("A user connected", socket.id);
+
+//   const userId = socket.handshake.query.userId;
+//   if (userId) userSocketMap[userId] = socket.id;
+
+//   // io.emit() is used to send events to all the connected clients
+//   io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+//   socket.on("disconnect", () => {
+//     console.log("A user disconnected", socket.id);
+//     delete userSocketMap[userId];
+//     io.emit("getOnlineUsers", Object.keys(userSocketMap));
+//   });
+// });
+
+// export { io, app, server };
+
+
+
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
@@ -11,12 +51,12 @@ const io = new Server(server, {
   },
 });
 
+// Used to store online users
+const userSocketMap = {}; // {userId: socketId}
+
 export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
-
-// used to store online users
-const userSocketMap = {}; // {userId: socketId}
 
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
@@ -24,8 +64,32 @@ io.on("connection", (socket) => {
   const userId = socket.handshake.query.userId;
   if (userId) userSocketMap[userId] = socket.id;
 
-  // io.emit() is used to send events to all the connected clients
+  // Notify all clients about online users
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  // Typing event - notify receiver that sender is typing
+  socket.on("typing", ({ toUserId }) => {
+    const receiverSocketId = userSocketMap[toUserId];
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("typing", { fromUserId: userId });
+    }
+  });
+
+  // Stop typing event - notify receiver that sender stopped typing
+  socket.on("stopTyping", ({ toUserId }) => {
+    const receiverSocketId = userSocketMap[toUserId];
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("stopTyping", { fromUserId: userId });
+    }
+  });
+
+  // Message seen event - notify sender that their message was seen
+  socket.on("messageSeen", ({ messageId, fromUserId }) => {
+    const senderSocketId = userSocketMap[fromUserId];
+    if (senderSocketId) {
+      io.to(senderSocketId).emit("messageSeen", { messageId, seenBy: userId });
+    }
+  });
 
   socket.on("disconnect", () => {
     console.log("A user disconnected", socket.id);
